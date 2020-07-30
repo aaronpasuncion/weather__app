3a9005960e724f982c95cf9809f00c308943bed2
Aaron P. Asuncion <aaronpasuncion@gmail.com>
2020-07-30 11:26:55 -0400

    converted majority of data from the api to desired specs. icons to be implemented next for weather

---
+++ src/js/base.js
@@ -0,0 +1,18 @@
+export const elements = {
+  // current weather
+  currentDate: document.querySelector(".current-day__date"),
+  currentTemp: document.querySelector(".current-day__cur-temp"),
+  currentWeather: document.querySelector(".current-day__desc"),
+  currentIcon: document.querySelector(".current-day__img"),
+  // current stats
+  curFeelsLike: document.querySelector(".current-day__feels-like--val"),
+  curHumidity: document.querySelector(".current-day__humidity--val"),
+  curWinds: document.querySelector(".current-day__winds--val"),
+  // hourly weather
+  hourlyForecastContainer: document.querySelector(".current-day__hourly"),
+
+  // 7 day forecast
+  upcomingWeatherContainer: document.querySelector(
+    ".upcoming-weather__container"
+  ),
+};
